40dd4c3bc063135d1e6067ff652cc837d76a1292
khánh anh nguyễn <khanhanh0205@gmail.com>
2020-06-25 23:30:53 +0700

    addnew

--- CourseP3/Models/IdentityModels.cs
+++ CourseP3/Models/IdentityModels.cs
@@ -43,6 +43,10 @@ namespace CourseP3.Models
         public System.Data.Entity.DbSet<CenterDetail> CenterDetails { get; set; }
     
         public System.Data.Entity.DbSet<Course> Courses { get; set; }
+        public System.Data.Entity.DbSet<News> News { get; set; }
+        public System.Data.Entity.DbSet<Semester> Semesters { get; set; }
+        public System.Data.Entity.DbSet<StudentCourse> StudentCourses { get; set; }
+
 
     }
 }
