97d81e2c44b95a9ce80b2038207919bbc0ba90fa
maintth1807076 <maintth1807076@fpt.edu.vn>
2020-06-25 22:06:03 +0700

    change model

---
+++ CourseP3/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CourseP3.Models
+{
+    public class Student
+    {
+        public string Fullname { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ CourseP3/Models/StudentCourse.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Web;
+using CourseP3.Areas.Admin.Models;
+
+namespace CourseP3.Models
+{
+    public class StudentCourse
+    {
+        [Key]
+        public int Id { get; set; }
+        public string StudentId { get; set; }
+        public int CourseId { get; set; }
+        public int Point { get; set; }
+        public StudentCourseStatus Status { get; set; }
+        public virtual Course Course { get; set; }
+        public virtual ApplicationUser Student { get; set; }
+
+        public enum StudentCourseStatus
+        {
+            Completed = 1,
+            Learning = 0,
+            Active = 2
+
+        }
+
+    }
+}
